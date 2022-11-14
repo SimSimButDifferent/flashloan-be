6b3310ec1ea0d282b416313d4adfb80093dbf1d5
simsim <simeon.campbell@live.com>
2022-11-14 14:12:20 +0700

    deployed goerli

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Following a flash loan tutorial from BlockExplorer
+
+0x782b3A7AfeCd0Cd438Eb21b869a8F3Be4A59467f -- goerli contract
--- contracts/Flashloan.sol
+++ contracts/Flashloan.sol
@@ -1,63 +1,57 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10; // .10 because thats the version a lot of the aave contracts that will be used are on.
 
-import {FlashLoanSimpleReceiverBase} from "https://github.com/aave/aave-v3-core/...";
-import {IPoolAddressesProvider} from "https://github.com/aave/aave-v3-core/...";
-import {IERC20} from "https://github.com/aave/aave-v3-core/...";
+import {FlashLoanSimpleReceiverBase} from "@aave/core-v3/contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol";
+import {IPoolAddressesProvider} from "@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IERC20} from "@aave/core-v3/contracts/dependencies/openzeppelin/contracts/IERC20.sol";
 
 contract FlashLoan is FlashLoanSimpleReceiverBase {
     address payable owner;
 
-    constructor(address _addressProvider) FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider)) {
-        owner = payable(msg.sender)
+    constructor(address _addressProvider)
+        FlashLoanSimpleReceiverBase(IPoolAddressesProvider(_addressProvider))
+    {
+        owner = payable(msg.sender);
     }
 
     function executeOperation(
-    address asset,
-    uint256 amount,
-    uint256 premium,
-    address initiator,
-    bytes calldata params
-  ) external override returns (bool) {
-    // we have the borrowed funds
-    // Logic
-    uint256 amountOwed = amount + premium;
-    IERC20(asset).approve(address(POOL), amountOwed);
+        address asset,
+        uint256 amount,
+        uint256 premium,
+        address initiator,
+        bytes calldata params
+    ) external override returns (bool) {
+        // we have the borrowed funds
+        // Logic
+        uint256 amountOwed = amount + premium;
+        IERC20(asset).approve(address(POOL), amountOwed);
 
-    return true;
-  }
+        return true;
+    }
 
-  function requestFlashLoan(address _token, uint256 _amount) {
-    address recieverAddress = address(this);
-    address asset = _token;
-    uint256 amount = _amount;
-    bytes memory params = "";
-    uint16 referralCode = 0;
+    function requestFlashLoan(address _token, uint256 _amount) public {
+        address recieverAddress = address(this);
+        address asset = _token;
+        uint256 amount = _amount;
+        bytes memory params = "";
+        uint16 referralCode = 0;
 
-
-    POOL.flashLoanSimple(
-        receiverAddress,
-        asset,
-        amount,
-        params,
-        referralCode
-        );
+        POOL.flashLoanSimple(recieverAddress, asset, amount, params, referralCode);
     }
 
     function getBalance(address _tokenAddress) external view returns (uint256) {
-      return IERC20(_tokenAddress).balanceOf(address(this));
+        return IERC20(_tokenAddress).balanceOf(address(this));
     }
 
     function withdraw(address _tokenAddress) external onlyOwner {
-      IERC20 token = IERC20(_tokenAddress);
-      token.transfer(msg.sender, token.balanceOf(address(this)));
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
     modifier onlyOwner() {
-      require(msg.sender == owner, "Only the contract owner can call this function");
-      _;
+        require(msg.sender == owner, "Only the contract owner can call this function");
+        _;
     }
 
     receive() external payable {}
-
 }
