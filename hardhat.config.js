f6871f9883e988e17d0c2d4da67f565224478a2a
simsim <simeon.campbell@live.com>
2022-11-15 12:53:43 +0700

    contract verified

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,23 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
+
+const INFURA_GOERLI_URL = process.env.INFURA_GOERLI_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.10",
-  networks: {
-    goerli: {
-      url: process.env.INFURA_GOERLI_URL,
-      accounts: [process.env.PRIVATE_KEY],
+    solidity: "0.8.10",
+    networks: {
+        goerli: {
+            url: INFURA_GOERLI_URL,
+            accounts: [PRIVATE_KEY],
+        },
     },
-  },
-};
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+}
